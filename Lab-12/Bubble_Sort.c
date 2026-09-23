a54aba0545ea1d327463b73c69586ed7814223d0
hemangvaghela6607 <hemangvaghela6607@gmail.com>
2026-09-23 21:52:05 +0530

    initial commit

--- Lab-12/Bubble_Sort.c
+++ Lab-12/Bubble_Sort.c
@@ -1,33 +1,37 @@
-#include <stdio.h>
+#include<stdio.h>
+
+void bubble_sort(int arr[], int n) {
+  int i, j, temp;
+  for (i=0;i<n-1;i++)
+    {
+    for (j=0;j<n-i-1;j++)
+    {
+      if (arr[j]>arr[j+1])
+      {
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
+
 
 int main() {
-    int n, i, j, temp;
+  int array[100], search, i, n;
 
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
+  printf("\nEnter the Length of Array:=> ");
+  scanf("%d",&n);
+  printf("\nEnter Elements of Array:\n");
+      for(i=0;i<n;i++)
+      {
+	    printf("\nA[%d]= ",i);
+	    scanf("%d",&array[i]);
+      }
 
-    int arr[n];
+  bubble_sort(array, n);
 
-    printf("Enter %d elements:\n", n);
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // Bubble Sort
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    printf("Sorted array in ascending order:\n");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    return 0;
+  printf("\nSorted Array: ");
+  for(i=0;i<n;i++)
+        printf("%d  ",array[i]);
 }
